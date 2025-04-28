5eb91a5bc83c2ec1bed46767a23d8f04b0587b89
AtamjonRaximov <raximovatam999@gmail.com>
2025-04-28 09:59:48 +0500

    add app.js

---
+++ app.js
@@ -0,0 +1,84 @@
+// let server = require("http") 
+
+// server.createServer((sorov,javob)=>{
+//     javob.write("hey boy")
+
+
+//     javob.end()
+// }).listen(8000)
+ 
+// const http = require("http");
+
+// http.createServer((request, response) => {
+//     if (request.url != "/favicon.ico") {
+//         console.log(request.url);
+
+//         response.writeHead(200, { "Content-Type": "text/html" });
+//         response.write("text");
+//         response.end();
+//     }
+// }).listen(8000);
+
+
+
+
+// const http = require("http");
+// const fs = require("fs");
+
+// http.createServer((request, response) => {
+//     if (request.url == "/favicon.ico") return;
+
+//    else if (request.url == "/page1") {
+//         response.writeHead(200, { "Content-Type": "text/html" });
+//         response.write("1");
+//         response.end();
+//         return;
+//     }
+
+//     else if (request.url == "/page2") {
+//         response.writeHead(200, { "Content-Type": "text/html" });
+//         response.write("2");
+//         response.end();
+//         return;
+//     }
+
+//     else if (request.url == "/page3") {
+//         response.writeHead(200, { "Content-Type": "text/html" });
+//         response.write("3");
+//         response.end();
+//         return;
+//     }
+    
+
+//     fs.readFile("index.html", (error, data) => {
+//         if (!error) {
+//             response.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+//             response.end(data);
+//         } else {
+//             response.writeHead(404, { "Content-Type": "text/plain" });
+//             response.end("Fayl topilmadi");
+//         }
+//     });
+
+// }).listen(8000);
+
+
+
+const http = require("http")
+const fs = require("fs")
+
+http.createServer(async(request,response)=>{
+    if(request.url != "/favicon.ico"){
+        let text = await fs.promises.readFile("index.html","utf8");
+        response.writeHead(200,{"Content-Type":"text/html"});
+        response.write(text);
+        response.end();
+    }
+
+}).listen(3000)
+
+let obj ={
+    '/page1': 'file1.html',
+	'/page2': 'file2.html',
+	'/page3': 'file3.html',
+}
