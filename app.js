3a1151d7465345196e393673e9fb7787a740ae95
AtamjonRaximov <raximovatam999@gmail.com>
2025-05-05 09:30:09 +0500

    add .

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-// let server = require("http") 
+// let server = require("http")
 
 // server.createServer((sorov,javob)=>{
 //     javob.write("hey boy")
 
-
 //     javob.end()
 // }).listen(8000)
- 
+
 // const http = require("http");
 
 // http.createServer((request, response) => {
@@ -18,9 +17,6 @@
 //         response.end();
 //     }
 // }).listen(8000);
-
-
-
 
 // const http = require("http");
 // const fs = require("fs");
@@ -48,7 +44,6 @@
 //         response.end();
 //         return;
 //     }
-    
 
 //     fs.readFile("index.html", (error, data) => {
 //         if (!error) {
@@ -62,23 +57,66 @@
 
 // }).listen(8000);
 
+// const http = require("http")
+// const fs = require("fs")
+
+// http.createServer(async(request,response)=>{
+//     if(request.url != "/favicon.ico"){
+//         let text = await fs.promises.readFile("index.html","utf8");
+//         response.writeHead(200,{"Content-Type":"text/html"});
+//         response.write(text);
+//         response.end();
+//     }
+
+// }).listen(3000)
+
+// let obj ={
+//     '/page1': 'file1.html',
+// 	'/page2': 'file2.html',
+// 	'/page3': 'file3.html',
+// }
+
+// const http = require("http");
+// const url = require("url");
+
+// http.createServer(
+//     function(request, response) {
+//         if (request.url !== "/favicon.ico") {
+//             console.log( url.parse(request.url, true).query); 
+//             response.end();
+//         }
+        
+        
+ 
+//   }).listen(8000);
 
 
-const http = require("http")
-const fs = require("fs")
 
-http.createServer(async(request,response)=>{
-    if(request.url != "/favicon.ico"){
-        let text = await fs.promises.readFile("index.html","utf8");
-        response.writeHead(200,{"Content-Type":"text/html"});
-        response.write(text);
-        response.end();
-    }
+// const http = require("http");
+// const fs = require("fs");
+  
+// http.createServer(async (request, response) => {
+        
+//     if(request.url == "/user"){
+           
+//           let body = "";  
+//           for await (const chunk of request) {
+//             body += chunk;
+//           }
+//         let userName = "";
+//         let userAge = 0;
+//         const params = body.split("&");
+//         for(param of params){
+//             const [paramName, paramValue] = param.split("=");
+//             if(paramName === "username") userName = paramValue;
+//             if(paramName === "userage") userAge = paramValue;
+//         }
+//         response.end(`Your name: ${userName}  Your Age: ${userAge}`);
+//     }
+//     else{
+//         fs.readFile("index.html", (_, data) => response.end(data));
+//     }
+// }).listen(8000, ()=>console.log("Сервер запущен по адресу http://localhost:8000"));
 
-}).listen(3000)
 
-let obj ={
-    '/page1': 'file1.html',
-	'/page2': 'file2.html',
-	'/page3': 'file3.html',
-}
+
